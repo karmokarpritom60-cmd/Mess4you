bae3dad14683cb325599a998e01765b9d822742b
Pritom Karmokar <karmokarpritom60@gmail.com>
2026-05-18 01:50:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
